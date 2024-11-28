825343fe3a3f99527c2fa691abb16bc99dfb4c5a
rhoeunbin <rhoeb21@gmail.com>
2024-11-28 16:44:43 +0900

    🔧project settings

---
+++ src/main/java/com/rhonnbin/hanbook/HanBookApplication.java
@@ -0,0 +1,13 @@
+package com.rhonnbin.hanbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rhonnbin/hanbook/HanBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhonnbin.hanbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
